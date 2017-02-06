2c61b80de7032f609d20d320add53011a5d9832f
liubeijing <1573018715@qq.com>
2017-02-06 18:38:21 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>shopping-cart</h1>
+    <hr/>
+    <ProductList/>
+    <hr/>
+    <Cart/>
+  </div>
+</template>
+
+<script>
+import ProductList from './components/ProductList'
+import Cart from './components/Cart'
+
+export default {
+  name: 'app',
+  components: {
+    ProductList,
+    Cart
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+       products
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+    export default{
+
+    }
+</script>
